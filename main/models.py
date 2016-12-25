81fcf746b15fbe46d258bd0e41289f44c9d969d6
Teddy Heidmann <ath55@cornell.edu>
2016-12-24 22:52:12 -0800

    Initialized models for main and rush

--- main/models.py
+++ main/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Officers(models.Model):
+    id = models.AutoField(db_index=True, max_length=4, primary_key=True)
+    position = models.TextField(max_length=100, default='')
+    name = models.TextField(max_length=100, default='', null=False)
+    email = models.EmailField(max_length=75, default='', null=False)
+    year = models.IntegerField(default='1909')
+    
+class Brothers(models.Model):
+    id = models.AutoField(db_index=True, max_length=4, primary_key=True)
+    first = models.CharField(db_index=True, max_length=100, default='')
+    last = models.CharField(db_index=True, max_length=100, default='')
+    major = models.TextField(max_length=100, default='')
+    home = models.TextField(max_length=100, default='')
+    year = models.IntegerField(default='1909')
+    pic = models.ImageField(default='main/img/brothers/no_img.jpeg')
+    active = models.BooleanField(default=True)
--- rush/models.py
+++ rush/models.py
@@ -1,5 +1,47 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Rushee(models.Model):
+    id = models.AutoField(db_index=True, primary_key=True)
+    first = models.CharField(db_index=True, max_length=100)
+    last = models.CharField(db_index=True, max_length=100)
+    netid = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
+    build = models.CharField(max_length=50)
+    room = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    latest_signin = models.IntegerField(default=0)
+    latest_signin_date = models.DateTimeField(default=datetime.now)
+    
+class Signin(models.Model):
+    id = models.AutoField(db_index=True, primary_key=True)
+    rid = models.ForeignKey(Rushee, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    qotd = models.CharField(max_length=100, default='')
+    ans = models.CharField(max_length=200, default='')
+    img = models.ImageField(upload_to='rush_pics/', default='/static/rush/no_img.jpeg')
+    img_small = models.ImageField(upload_to='rush_pics_small/', default='/static/rush/no_img.jpeg')
+
+class Comment(models.Model):
+    id = models.AutoField(db_index=True, primary_key=True)
+    rid = models.ForeignKey(Rushee, on_delete=models.CASCADE)
+    broid = models.IntegerField(default=0)
+    comment = models.CharField(max_length=10000)
+
+class Event(models.Model):
+    id = models.AutoField(db_index=True, primary_key=True)
+    rid = models.ForeignKey(Rushee, on_delete=models.CASCADE)
+    event = models.CharField(max_length=100)
+
+class Settings(models.Model):
+    name = models.CharField(db_index=True, max_length=100, primary_key=True)
+    char = models.CharField(max_length=100, default='')
+    val = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now=True)
+
+class Brothers(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.IntegerField(default=0)
+    comments = models.BooleanField(default=True)
